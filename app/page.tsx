330699ca66c19f0d83661e24d99c1eeb12c5b489
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-18 17:57:20 +0500

    n

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic"; // Force dynamic rendering
 export default async function Home() {
   const userInfo = await fetchUserAction();
 
