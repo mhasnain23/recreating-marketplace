15e3a425e7b9504236a1127b2bc6fc7c6d74fba4
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-18 17:28:13 +0500

    n

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
-const DB_CONNECT_URI = process.env.MONGODB_URI!;
 
-if (!DB_CONNECT_URI) {
-    throw new Error('Please define the DB_CONNECT_URI environment variable inside .env');
-}
 
 // Cached connection type
 
 async function connectDB() {
     try {
-        const conn = await mongoose.connect(DB_CONNECT_URI);
-
+        const conn = await mongoose.connect(process.env.MONGODB_URI!);
         if (conn) {
             console.log("Connected to MongoDB");
         } else {
