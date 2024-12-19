750ec04283e36db56a2764f385ae5dc6523ac10e
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-19 07:50:18 +0500

    updated

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  const connectionURL: string | undefined = process.env.MONGODB_URL!;
+// const connectDB = async () => {
+//   const connectionURL: string | undefined = process.env.MONGODB_URL!;
 
-  mongoose
-    .connect(connectionURL)
-    .then(() => console.log("Job board connection is successfull"))
-    .catch((e) => console.log(e));
-};
+//   mongoose
+//     .connect(connectionURL)
+//     .then(() => console.log("Job board connection is successfull"))
+//     .catch((e) => console.log(e));
+// };
 
-export default connectDB;
-
-
+// export default connectDB;
 
 
 
@@ -20,18 +18,20 @@ export default connectDB;
 
 
 
-// export default async function connectDB() {
-//     const uri = process.env.MONGODB_URI;
 
-//     if (!uri) {
-//         throw new Error("❌ MONGODB_URI is not defined in the environment variables");
-//     }
 
-//     try {
-//         await mongoose.connect(uri);
-//         console.log("✅ Connected to MongoDB");
-//     } catch (error: any) {
-//         console.error("❌ Error connecting to MongoDB:", error.message);
-//         throw error; // Rethrow the error if you want to handle it in the caller
-//     }
-// }
+export default async function connectDB() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("❌ MONGODB_URI is not defined in the environment variables");
+  }
+
+  try {
+    await mongoose.connect(uri);
+    console.log("✅ Connected to MongoDB");
+  } catch (error: any) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    throw error; // Rethrow the error if you want to handle it in the caller
+  }
+}
